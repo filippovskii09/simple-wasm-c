5e235eff17cb8db4d9f01342c0e5690cf2acffc0
artur.filippovskii <arturfilippovskij@gmail.com>
2026-03-21 10:50:37 +0200

    add linting & refactor code for strong rules

--- Makefile
+++ Makefile
@@ -4,13 +4,21 @@ TARGET = ./build/main
 
 .PHONY: run clean wasm
 
+run-strong: src/main.c
+	mkdir -p ./build
+	$(CC) $(CFLAGS) src/main.c src/cJSON.c -Iinclude -o $(TARGET)
+	$(TARGET)
+
 run: src/main.c
 	mkdir -p ./build
-	$(CC) $(CFLAGS) src/main.c -o $(TARGET)
+	$(CC) src/main.c src/cJSON.c -Iinclude -o $(TARGET)
 	$(TARGET)
 
 clean:
 	rm -rf $(TARGET)
 
 wasm:
-	emcc -O3 -s WASM=1 -s EXPORTED_RUNTIME_METHODS='["cwrap"]' src/main.c -o ./www/main.js
+	emcc -O3 -s WASM=1 -s EXPORTED_RUNTIME_METHODS='["cwrap"]' src/main.c src/cJSON.c -Iinclude -o ./www/main.js
+
+lint:
+	/opt/homebrew/opt/llvm/bin/clang-tidy src/main.c -- -Iinclude -isysroot $(shell xcrun --show-sdk-path)
--- src/main.c
+++ src/main.c
@@ -1,21 +1,70 @@
 #include <stdio.h>
 #include <string.h>
+#include "../include/cJSON.h"
 
-#define SHOP_SIZE 3
-#define PRODUCT_NAME_MAX_LEN 40
+enum {
+	SHOP_SIZE = 3,
+	PRODUCT_NAME_MAX_LEN = 40,
+	BUFFER_SIZE = 1024
+};
 
+/*
+ * In this case, we use double instead of float because it is more precise.
+ * You can use float instead of double if you want, but it is not recommended(for prices).
+ * If you want to use float, you must write 799.99f instead of 799.99.
+ * Float type have size 4 bytes(32 bits), double type have size 8 bytes(64 bits).
+*/
 struct Product {
 	int id;
 	char name[PRODUCT_NAME_MAX_LEN];
-	float price;
+	double price; // float price;
 };
 
 int main(void) {
-	struct Product shop[SHOP_SIZE] = {
-		{101, "Smartphone", 799.99},
-		{102, "Laptop",     1250.00},
-		{103, "Headphones", 150.50}
-	};
+	FILE *fptr;
+
+	fptr = fopen("fake_data/product.json", "r");
+
+	if(fptr == NULL) {
+		printf("Error opening file\n");
+		return 1;
+	}
+
+	struct Product shop[SHOP_SIZE];
+
+	char buffer[BUFFER_SIZE];
+	size_t n = fread(buffer, sizeof(char), BUFFER_SIZE, fptr);
+	buffer[n] = '\0';
+
+	cJSON *json = cJSON_Parse(buffer);
+	if(json == NULL) {
+		printf("Error parsing JSON\n");
+		return 1;
+	}
+
+	cJSON *products = cJSON_GetObjectItem(json, "products");
+	if(products == NULL) {
+		printf("Error getting products array\n");
+		return 1;
+	}
+
+	int array_size = cJSON_GetArraySize(products);
+
+  for (int i = 0; i < array_size; i++) {
+		cJSON *product = cJSON_GetArrayItem(products, i);
+		
+		cJSON *id_obj    = cJSON_GetObjectItem(product, "id");
+		cJSON *name_obj  = cJSON_GetObjectItem(product, "name");
+		cJSON *price_obj = cJSON_GetObjectItem(product, "price");
+		
+		shop[i].id = id_obj->valueint;
+		strcpy(shop[i].name, name_obj->valuestring);
+		shop[i].price = price_obj->valuedouble;
+	}
+
+	cJSON_Delete(json);
+
+	printf("Read %zu products\n", n);
 
 	struct Product *ptr = shop;
 
